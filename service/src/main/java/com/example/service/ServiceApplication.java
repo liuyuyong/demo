38392bd5b5823370b92bc55ad4b7fa882bcecf99
liuyuyong <liuyuyong_lucky@163.com>
2019-05-12 17:38:13 +0800

    1&2&4
    1&2
    
    
    4
    
    1&2

--- service/src/main/java/com/example/service/ServiceApplication.java
+++ service/src/main/java/com/example/service/ServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * liu
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class ServiceApplication {
